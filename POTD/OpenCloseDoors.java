b32dbfee9ef0ad6523208d7e557ab4afb201a8f3
dilip0418 <sudheer0418@gmail.com>
2024-05-12 19:40:08 +0530

    toggleDoorStatus added problem statement edited

--- POTD/OpenCloseDoors.java
+++ POTD/OpenCloseDoors.java
@@ -2,7 +2,10 @@ package POTD;
 
 /* 
  * Problem statement
-There are ‘N’ doors and ‘N’ people in a house. Each person and door has a unique ID ranging from 1 to ‘N’. A person can change the status of the door i.e, if the door is open then a person can close the door and vice versa. Initially, all the doors are closed and each person wants to change the status of all doors whose ID is a multiple of the ID of the person. You need to find out the final status of all the doors.
+There are ‘N’ doors and ‘N’ people in a house. Each person and door has a unique ID ranging from 1 to ‘N’. 
+A person can change the status of the door i.e, if the door is open then a person can close the door and vice versa. 
+Initially, all the doors are closed and each person wants to change the status of all doors whose ID 
+is a multiple of the ID of the person. You need to find out the final status of all the doors.
 
 The answer should be given in a form of a binary string where ‘0’ represents the closed door and ‘1’ represents the open door. For example, the status “close open close” will form a binary string “010”.
  */
