206c865d2e4371b5fccb7340df24b783021085d7
dilip0418 <sudheer0418@gmail.com>
2024-05-13 16:36:03 +0530

    rotate the array by d places to the right or left

--- Array/Easy/RightRotateArrayByDPlaces.java
+++ Array/Easy/RightRotateArrayByDPlaces.java
@@ -4,38 +4,33 @@ public class RightRotateArrayByDPlaces {
     public static void main(String[] args) {
         int arr[] = new int[] { 1, 2, 3, 4, 5 };
         // rightRotatebyDplaces(arr, 2);
-        leftRoateByDPlaces(arr, 2);
+        // leftRoateByDPlaces(arr, 2);
+        rightRotatebyDplaces(arr, 2);
         for (int i : arr) {
             System.out.print(i + " ");
         }
     }
 
-    private static int[] rightRotatebyDplaces(int[] arr, int d) {
-        if (d == 0 || d == arr.length)
-            return arr;
-        int i = 0, temp;
-        while (i < d) {
-            temp = arr[i];
-            arr[i] = arr[(d + i) % arr.length];
-            arr[(d + i) % arr.length] = temp;
-            // d--;
-            i++;
-        }
-        return arr;
+    private static void rightRotatebyDplaces(int[] arr, int d) {
+        int n = arr.length;
+        // normalize the d value to lie withing length of the array
+        if (d > n)
+            d %= n;
+        reverse(arr, 0, n);
+        reverse(arr, 0, d);
+        reverse(arr, d, n);
     }
 
     private static void leftRoateByDPlaces(int[] arr, int d) {
         int n = arr.length;
-        d %= n;
         if (d > n)
-            return;
+            d %= n;
         reverse(arr, 0, d);
         reverse(arr, d, n);
         reverse(arr, 0, n);
     }
 
     private static void reverse(int[] arr, int start, int end) {
-
         for (int i = start, j = end - 1; i < j; i++, j--) {
             int temp = arr[i];
             arr[i] = arr[j];
